702108c1e9af539f5ce75e4904d0a90167f19101
Tony Roman <rony@cacti.net>
2018-08-21 09:13:34 -0400

    Update markdownlint style with notes

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -3,4 +3,8 @@ all
 rule "MD013", code_blocks: false, tables: false
 rule "MD029", style: "ordered"
 rule "MD046", style: "fenced"
+
+# Rule Exclusions
+exclude_rule "MD001" # Headers are useful in other ways
+exclude_rule "MD024" # Headers with same name are useful, but break link labeling (Rework needed on affected files before enabling this rule)
 exclude_rule "MD046" # seems broken
