1eb640d5e01f27776824232417229b2e42204e2f
netniV <netniv@hotmail.com>
2018-09-24 23:12:34 +0000

    Add script to notify of missing links

---
+++ check_missing_link.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#  +-------------------------------------------------------------------------+
+#  | Copyright (C) 2004-2018 The Cacti Group                                 |
+#  |                                                                         |
+#  | This program is free software; you can redistribute it and/or           |
+#  | modify it under the terms of the GNU General Public License             |
+#  | as published by the Free Software Foundation; either version 2          |
+#  | of the License, or (at your option) any later version.                  |
+#  |                                                                         |
+#  | This program is distributed in the hope that it will be useful,         |
+#  | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+#  | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+#  | GNU General Public License for more details.                            |
+#  +-------------------------------------------------------------------------+
+#  | http://www.cacti.net/                                                   |
+#  +-------------------------------------------------------------------------+
+#
+# Helper shell script to all documentation is linked from index
+
+# Determine if running script with out arguments so that path will be relative
+if [ -n "${*}" ]
+then
+
+	FILENAME=`basename $1`
+	LINES=`grep "${FILENAME}" README.md | wc -l`
+	if [ ${LINES} -gt 0 ]; then
+		if [ ! -z ${CHECK_SHOW_LINKED} ]; then
+			echo "Linked....: ${FILENAME}";
+		fi
+	else
+		echo "Not Linked: ${FILENAME}"
+	fi
+	exit ${LINES};
+else
+	LINES=`find -name \*.md ! -name README.md -exec "$0" "{}" \;`
+	COUNT=`echo "${LINES}" | grep "Not Linked:" | wc -l`
+
+	if [ -n ${CHECK_SHOW_LINKED} ] || [ ${COUNT} -gt 0 ]; then
+		echo "${LINES}";
+		echo "";
+		echo "$COUNT unlinked files";
+	fi
+	exit $COUNT;
+fi
